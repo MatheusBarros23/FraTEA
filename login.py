583d1b7c2cdfb342da1d8f85bc59e0825d98a11f
Vitor Carvalho <vitor.velozo2003@gmail.com>
2021-11-09 12:15:07 -0300

    create login.py

---
+++ login.py
@@ -0,0 +1,12 @@
+print(13*'=' + '\nVAMOS COMEÇAR\n' + 13*'=')
+print('\nOi, eu sou o FraTEA!\n\nEu vou ser o seu guia nessa aventura incrível!\n')
+
+username = input('Qual o seu nome? ')
+
+list_avatar = [1, 2, 3]
+chosen_avatar = int(input((f'{username}, antes de começar, escolha seu avatar:\n(digite um número de 1 a 3) ')))
+
+print('''Pronto!\nAgora vamos aprender a jogar: \n
+Neste jogo, nós vamos aprender sobre alguns dos elementos básicos que formam as sentenças do português.\n
+Você só precisa arrastar as imagens para os seus lugares correspondentes nas sentenças.\n
+Vamos aprender brincando!\n''')
