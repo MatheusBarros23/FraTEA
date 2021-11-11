5147a7b5189d095060aac2d6cf5adbb2743487ed
MatheusBarros23 <mprb@cesar.school>
2021-11-11 12:56:19 -0300

    delete game02.py old

--- game02.py
+++
@@ -1,9 +0,0 @@
-import random
-
-subjects = ['CARLOS', 'MATHEUS', 'ANA']
-verbs = ['COMEU', 'GOSTA DE', 'COMPROU']
-object = ['MAÇÃ', 'PIZZA', 'FEIJÃO']
-
-answer = (f'{random.choice(subjects)} {random.choice(verbs)} {random.choice(object)}')
-
-print(answer)
